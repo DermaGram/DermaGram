2fc8eae75d3d04372db6cfa62c71b6747f20dfca
ShoniB <32283101+ShoniB@users.noreply.github.com>
2017-10-17 19:10:53 -0400

    Update dbScript.sql

--- master/db/dbScript.sql
+++ master/db/dbScript.sql
@@ -1,12 +1,8 @@
 CREATE DATABASE Dermagram;
 USE Dermagram;
 
-CREATE TABLE DG_User(username VARCHAR(50) NOT NULL, password  VARCHAR(50), email  VARCHAR(50), firstName  VARCHAR(50)
-, lastName VARCHAR(50), sex CHAR(1), age INT, location VARCHAR(50),  PRIMARY KEY(username));
-
-CREATE TABLE Image(link VARCHAR(200), username_fk VARCHAR(50), FOREIGN KEY(username_fk) REFERENCES DG_User(username))
-;
-
+CREATE TABLE DG_User(username VARCHAR(50) NOT NULL, password  VARCHAR(500), email  VARCHAR(50), firstName  VARCHAR(50)
+, lastName VARCHAR(50), albumLink VARCHAR(200),  PRIMARY KEY(username));
 
 
 
