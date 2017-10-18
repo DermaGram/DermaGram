8437de9bd1d27eab65cd15759903e2fff87de7b6
ShoniB <32283101+ShoniB@users.noreply.github.com>
2017-10-17 20:00:34 -0400

    Update registrationClass.py
    Added functionality to check email input for a valid email address format

--- master/db/registrationClass.py
+++ master/db/registrationClass.py
@@ -1,5 +1,6 @@
 ### added for signin page
 from wtforms import Form, BooleanField, TextField, PasswordField, validators
+from wtforms.validators import Email
 from passlib.hash import sha256_crypt
 from MySQLdb import escape_string as thwart
 import gc
@@ -7,7 +8,7 @@ import gc
 
 class RegistrationForm(Form):
     username = TextField('Enter a username', [validators.Length(min=6, max=50)])
-    email = TextField('Enter your email', [validators.Length(min=6, max=50)])
+    email = TextField('Enter your email', [validators.Length(min=6, max=50),validators.Email("Not a valid email address")])
     password = PasswordField('Enter a password', [
         validators.Required(),
         validators.EqualTo('confirm', message='Your passwords must match')
