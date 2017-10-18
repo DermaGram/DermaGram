2ffe5a8d0d6d668b1f4b31438dd8e1f155629f8e
ShoniB <32283101+ShoniB@users.noreply.github.com>
2017-10-18 10:43:36 -0400

    Update registrationClass.py
    Added some comments. Required is not needed for the other inputs because there is a specified length

--- master/db/registrationClass.py
+++ master/db/registrationClass.py
@@ -1,11 +1,10 @@
-### added for signin page
+# Used with register.html, creates a form class to be used to pull data from the browser
 from wtforms import Form, BooleanField, TextField, PasswordField, validators
 from wtforms.validators import Email
 from passlib.hash import sha256_crypt
-from MySQLdb import escape_string as thwart
+from MySQLdb import escape_string as thwart # used in function register() in app.py
 import gc
-
-
+# define input fields and sets errors for bad input
 class RegistrationForm(Form):
     username = TextField('Enter a username', [validators.Length(min=6, max=50)])
     email = TextField('Enter your email', [validators.Length(min=6, max=50),validators.Email("Not a valid email address")])
