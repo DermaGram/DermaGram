6b58c3dc5541d203cb6370e76c7fdf53607bbee8
DermaGram <Andrew.Hearst75@myhunter.cuny.edu>
2017-10-23 22:05:17 -0400

    Update port

--- master/app.py
+++ master/app.py
@@ -117,4 +117,4 @@ def display_image():
 
 if __name__ == "__main__":
     app.secret_key = os.urandom(12)
-    app.run(debug=True,host='0.0.0.0', port=5002)
+    app.run(debug=True,host='0.0.0.0', port=5000)
