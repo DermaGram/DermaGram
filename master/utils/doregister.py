4ef285aed2210349c26052f2dbb707249069fd3e
ShoniB <32283101+ShoniB@users.noreply.github.com>
2017-10-30 22:43:57 -0400

    Update doregister.py
    Added return false at end of function

--- master/utils/doregister.py
+++ master/utils/doregister.py
@@ -3,6 +3,7 @@ from db.registrationClass import *
 from db.db import *
 
 def do_register(Form): 
+    status = False
     form = RegistrationForm(request.form)
     # use form from register.html and pull data from that form
     if request.method == "POST" and form.validate():
@@ -14,7 +15,8 @@ def do_register(Form):
         x = c.execute("SELECT username FROM DG_User WHERE username = (%s)", [thwart(username_p)])
         if int(x)>0: # if username exists 
             flash("That username is already taken, please choose another")
-            return False
+            status = False
+            return status
 
         else: # insert data into database 
             c.execute("INSERT INTO DG_User (username, password, email) VALUES (%s, %s, %s)", (username_p, password, email))
@@ -25,4 +27,6 @@ def do_register(Form):
             gc.collect()
             session['logged_in'] = True
             session['username'] = username_p
-            return True
+            status = True
+            return status
+    return status
