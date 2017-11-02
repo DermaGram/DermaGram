7a2008246814754b5453d9e20e7d46d2072936a1
DermaGram <Andrew.Hearst75@myhunter.cuny.edu>
2017-11-01 23:02:38 -0400

    Add homepage w/ links to Login SignUp and Profile.

--- master/utils/dologin.py
+++ master/utils/dologin.py
@@ -1,33 +1,66 @@
-from flask import Flask, flash, redirect, render_template, request, session, abort, url_for
+from flask import request
 from MySQLdb import escape_string as thwart
 from passlib.hash import sha256_crypt
 from db.db import connection
+from utils.session_utils import SessionUtils
 from utils.logging_utils import LoggingUtils
-
+import logging
 LoggingUtils.initialize_logger(__name__)
 
-def do_login():
-    logger = logging.getLogger(__name__)
-    username = str(request.form['username'])
-    password = str(request.form['password'])
-    logger.info( "Attempting login for {0}".format(username))
-    if username == "":
-        session['logged_in'] = False
-	logger.error("No username specified")
-	return
-    # fetch password
-    c, conn = connection()
-    name = c.execute("SELECT username FROM DG_User WHERE username = (%s)", [thwart(username)])
-    if int(name) == 0:
-	session['logged_in'] = False
-	logger.error("Username {0} doesn't exist".format(username))
-	return
-    pw = c.execute("SELECT password FROM DG_User WHERE username = (%s)", [thwart(password)])
-    pw = c.fetchone()[0]
-    # encrypt entered password and check against queried password
-    if sha256_crypt.verify(password, pw):
-        session['logged_in'] = True
-        session['username'] = username
-	logger.info("User {0} is logged in".format(username))
-    else:
-	logger.error("Incorrect password")
+class Login:
+
+    @classmethod
+    def do_login(cls):
+        logger = logging.getLogger(__name__)
+        success = False
+        c, conn = connection()
+
+        # fetch and validate username
+        username = str(request.form['username'])
+        success = cls._is_username_valid(c, username)
+
+        # fetch and validate password
+        if success:
+            password = str(request.form['password'])
+            success = cls._is_password_valid(c, username, password)
+
+        # fetch and validate album info
+        # TODO - fetch album_name && album_id
+
+        # update user session info
+        #TODO: replace album_name && album_id with whatever we get from db
+        SessionUtils.update_session_info(success, username, 'ImgurPythonTest', 'cHPkw')
+        return success
+
+    @staticmethod
+    def _is_username_valid(cursor, username):
+        logger = logging.getLogger(__name__)
+        success = True
+
+        if not username:
+            logger.error("Username is empty sting.")
+            success = False
+        else:
+            name = cursor.execute("SELECT username FROM DG_User WHERE username = (%s)", [thwart(username)])
+            if int(name) == 0:
+                logger.error("Username {0} doesn't exist".format(username))
+                success = False
+
+        return success
+
+    @staticmethod
+    def _is_password_valid(cursor, username, password):
+        logger = logging.getLogger(__name__)
+        success = False
+
+        pw = cursor.execute("SELECT password FROM DG_User WHERE username = (%s)", [thwart(username)])
+        pw = cursor.fetchone()[0]
+
+        # encrypt entered password and check against queried password
+        if sha256_crypt.verify(password, pw):
+            success = True
+        else:
+            logger.error("Password {0} is incorrect.".format(password))
+
+        return success
+
