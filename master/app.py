8704bfc21a3a25286253391d9ed632dace8f3cd6
DermaGram <Andrew.Hearst75@myhunter.cuny.edu>
2017-11-02 20:39:33 -0400

    Revert port back to 5000

--- master/app.py
+++ master/app.py
@@ -93,4 +93,4 @@ def upload():
 
 if __name__ == "__main__":
     app.secret_key = os.urandom(12)
-    app.run(debug=True,host='0.0.0.0', port=5002)
+    app.run(debug=True,host='0.0.0.0', port=5000)
