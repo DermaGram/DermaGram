799b08388c4f9f290260d477a237a70191421d8e
smitsky <carshine56@yahoo.com>
2017-11-28 20:13:00 -0500

    I added connectAttempt2

---
+++ master/static/connect29.js
@@ -0,0 +1,8 @@
+$(document).ready(() => {
+  console.log("Hello from the profile page");
+    $('#image_table_row').on('click', () => {
+      console.log("Hello from the image table row");
+
+  })
+
+})
